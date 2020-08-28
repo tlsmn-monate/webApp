3c82306454eea000c2f5ae58373a79a72f600be5
Marcelo Onate <marcelo.onate@gmail.com>
2020-08-28 00:52:10 -0400

    datos en firebase, servicio e productos, desplegando articulos en home, loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { InfoPaginaService } from './services/info-pagina.service';
+import { ProductosService } from './services/productos.service';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,8 @@ import { InfoPaginaService } from './services/info-pagina.service';
 export class AppComponent {
   title = 'webApp';
 
-  constructor( public paginaService: InfoPaginaService){
+  constructor( public paginaService: InfoPaginaService,
+    public productosService: ProductosService){
 
 
   }
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,24 +9,45 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
 
   constructor( private http: HttpClient) {
 
+this.cargarInfo();
+this.cargarEquipo();
    // console.log('info cargada');
 
-// Leer archivo json
+  // Leer archivo json
 
-    this.http.get('./assets/data/data-pages.json')
-    .subscribe( (resp: InfoPagina) => {
-      this.cargada = true;
-      this.info = resp;
-
-      // tslint:disable-next-line: no-string-literal
-      //console.log(resp['email']);
-
-      console.log(resp);
-    })
 
    }
+private cargarInfo(){
+  this.http.get('./assets/data/data-pages.json')
+  .subscribe( (resp: InfoPagina) => {
+    this.cargada = true;
+    this.info = resp;
+
+    // tslint:disable-next-line: no-string-literal
+    //console.log(resp['email']);
+
+  });
+}
+
+private cargarEquipo(){
+
+  this.http.get('https://angular-html-2bae6.firebaseio.com/equipo.json')
+  .subscribe( (resp: any) => {
+    this.equipo = resp;
+   
+
+    // tslint:disable-next-line: no-string-literal
+    //console.log(resp['email']);
+
+    //console.log(resp);
+  });
+
+
+}
+
 }
