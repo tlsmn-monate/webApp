1b9efb0cf7997e18b678a05d2734c801e4014a80
Marcelo Onate <marcelo.onate@gmail.com>
2020-08-29 12:45:17 -0400

    implementacion search y filtrado

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
+import { ProductosDescripcion } from '../../interfaces/producto-descripcion.interface';
 
 @Component({
   selector: 'app-item',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
 
-  constructor() { }
+  producto: ProductosDescripcion;
+  id: string;
+
+
+  constructor( private route: ActivatedRoute, 
+               public productoService: ProductosService) { }
 
   ngOnInit(): void {
+
+  this.route.params.subscribe( parametros => {
+
+    // console.log(parametros ['id']);
+    this.productoService.getProducto(parametros['id'])
+    .subscribe( (producto: ProductosDescripcion) => {
+
+      this.id = parametros['id'];
+      this.producto = producto;
+            console.log(producto);
+
+    });
+  });
+
+
   }
 
 }
